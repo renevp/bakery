2270434b2ac8638ecddab6ba1b8d2aebad262194
Rene Vallenilla <rene.vallenilla@gmail.com>
2016-11-11 16:21:04 +1100

    Clean up in examples

--- lib/order_factory.rb
+++ lib/order_factory.rb
@@ -4,7 +4,11 @@ module OrderFactory
   MIN_ORDER_QTY = 1
 
   def self.build(order_data, order_class = Order, item_class=OrderItem)
-    order_class.new( ( order_data.collect { |item| create_item(item, item_class) } ).compact )
+    order_class.new( create_items(order_data, item_class))
+  end
+
+  def self.create_items(order_data, item_class)
+    (order_data.collect { |item| create_item(item, item_class) } ).compact
   end
 
   def self.create_item(item, item_class)
